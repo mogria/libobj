86d64392b6363e19c8fc2053516af1855a4394db
Mogria <m0gr14@gmail.com>
2013-01-16 22:47:23 +0100

    fixed macros

--- include/libobj/macros.h
+++ include/libobj/macros.h
@@ -2,20 +2,20 @@
 #define LIBOBJ_MACROS_H
 
 #define CLASS(name) \
-struct #name _class { \
-  Class *class;
+struct name { \
+  Class *class
 
 #define END_CLASS(name, construct, destruct) \
 }; \
-Class  #name _class = { \
-  sizeof(struct  #name _class), \
+Class name## _class = { \
+  sizeof(struct name), \
   construct, \
   destruct \
 }; \
 \
-Class *name = &#name _class;
+Class *name = &name## _class;
 
-#define CONSTRUCTOR(name) void (name)(void *self, va_list *args)
-#define DESTRUCTOR(name) void (name)(void *self)
+#define CONSTRUCTOR(name) void name(void *self, va_list *args)
+#define DESTRUCTOR(name) void name(void *self)
 
 #endif /* LIBOBJ_MACROS_H */
